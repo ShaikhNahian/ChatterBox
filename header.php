2a73b216ea37f81e6bc6e78e7cf07e270dd31ee7
Shaikh Nahian <56013701+ShaikhNahian@users.noreply.github.com>
2021-09-22 04:12:31 +0600

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>CHATTERBOX</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="master/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 </head>
