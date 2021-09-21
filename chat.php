7e74333a11ed52f93bcca5ef2817621bbceefa2d
Shaikh Nahian <56013701+ShaikhNahian@users.noreply.github.com>
2021-09-22 04:30:39 +0600

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,11 +1,11 @@
 <?php 
   session_start();
-  include_once "php/config.php";
+  include_once "master/php/config.php";
   if(!isset($_SESSION['unique_id'])){
-    header("location: login.php");
+    header("location: master/login.php");
   }
 ?>
-<?php include_once "header.php"; ?>
+<?php include_once "master/header.php"; ?>
 <body>
   <div class="wrapper">
     <section class="chat-area">
@@ -16,11 +16,11 @@
           if(mysqli_num_rows($sql) > 0){
             $row = mysqli_fetch_assoc($sql);
           }else{
-            header("location: users.php");
+            header("location: master/users.php");
           }
         ?>
-        <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
-        <img src="php/images/<?php echo $row['img']; ?>" alt="">
+        <a href="master/users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+        <img src="master/php/images/<?php echo $row['img']; ?>" alt="">
         <div class="details">
           <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
           <p><?php echo $row['status']; ?></p>
@@ -47,7 +47,7 @@
     </section>
   </div>
 
-  <script src="javascript/chat.js"></script>
+  <script src="master/javascript/chat.js"></script>
 
 </body>
 </html>
