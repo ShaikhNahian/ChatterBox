cc44a8873c8eaf9356a7c3d81f2a3bd68155c293
Shaikh Nahian <56013701+ShaikhNahian@users.noreply.github.com>
2021-09-22 08:52:48 +0600

    Update users.php

--- php/users.php
+++ php/users.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include_once "config.php";
+    include_once "master/php/config.php";
     $outgoing_id = $_SESSION['unique_id'];
     $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
     $query = mysqli_query($conn, $sql);
@@ -8,7 +8,7 @@
     if(mysqli_num_rows($query) == 0){
         $output .= "No users are available to chat";
     }elseif(mysqli_num_rows($query) > 0){
-        include_once "data.php";
+        include_once "master/php/data.php";
     }
     echo $output;
 ?>
