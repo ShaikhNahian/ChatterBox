ce2cb21657ce0b2ee3a7ccd742f7261e824ef539
Shaikh Nahian <56013701+ShaikhNahian@users.noreply.github.com>
2021-09-22 08:29:17 +0600

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -8,13 +8,13 @@ form.onsubmit = (e)=>{
 
 continueBtn.onclick = ()=>{
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/login.php", true);
+    xhr.open("POST", "master/php/login.php", true);
     xhr.onload = ()=>{
       if(xhr.readyState === XMLHttpRequest.DONE){
           if(xhr.status === 200){
               let data = xhr.response;
               if(data === "success"){
-                location.href = "users.php";
+                location.href = "master/users.php";
               }else{
                 errorText.style.display = "block";
                 errorText.textContent = data;
