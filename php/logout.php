9953eee32bb5df637a0227d400e564dc971239ab
Shaikh Nahian <56013701+ShaikhNahian@users.noreply.github.com>
2021-09-22 08:49:02 +0600

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(isset($_SESSION['unique_id'])){
-        include_once "config.php";
+        include_once "master/php/config.php";
         $logout_id = mysqli_real_escape_string($conn, $_GET['logout_id']);
         if(isset($logout_id)){
             $status = "Offline now";
@@ -9,12 +9,12 @@
             if($sql){
                 session_unset();
                 session_destroy();
-                header("location: ../login.php");
+                header("location: master/login.php");
             }
         }else{
-            header("location: ../users.php");
+            header("location: master/users.php");
         }
     }else{  
-        header("location: ../login.php");
+        header("location: master/login.php");
     }
 ?>
